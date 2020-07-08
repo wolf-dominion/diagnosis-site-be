a6e14bdd2ca8cfbf3db30c08edc344190e5ef72a
wolf-dominion <hannah_drury@yahoo.com>
2020-07-07 20:02:58 -0600

    changed process.env.SECRET_KEY_BASE to ENV['SECRET_KEY_BASE']

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::API
-    
+
+    def secret
+        # 2nd option is for if this app is deployed/production, 1st is for dev
+        Rails.application.secrets.secret_key_base || Rails.application.secret_key_base
+    end
+
     def authenticate
         authorization_header = request.headers["Authorization"]
-        
+
         if authorization_header
             token = authorization_header.split(" ")[1]
             begin
-                # 2nd option is for if this app is deployed/production, 1st is for dev
-                secret = Rails.application.secrets.secret_key_base || process.env.SECRET_KEY_BASE
                 @user_id = JWT.decode(token, secret)[0]["user_id"]
             rescue
                 render json: {message: "The credentials are wrong"}, status: :unauthorized
@@ -19,7 +22,6 @@ class ApplicationController < ActionController::API
 
     def createToken(user)
         payload = { user_id: user.id }
-        secret = Rails.application.secrets.secret_key_base || process.env.SECRET_KEY_BASE
         JWT.encode(payload, secret)
     end
 
